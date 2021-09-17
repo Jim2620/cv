41692bccb1651ccfb7958079579b0cebd9e214f4
Jim Liriano <jim.liriano@gmail.com>
2021-09-17 09:53:02 -0400

    added base files and tried to establish routing

---
+++ src/components/pages/education/Education.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class education extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
---
+++ src/components/pages/experience/Experience.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Experience extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
